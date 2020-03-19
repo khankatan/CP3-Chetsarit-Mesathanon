c31780c81b4718a6130ca0b789a63385babcaabc
khankatan <khan71037@gmail.com>
2020-03-20 01:24:43 +0700

    add score

---
+++ assignments/Exercise4_Chetsarit_M.py
@@ -0,0 +1,10 @@
+fe = 80
+gb = 96
+ics = 98
+cp = 92
+
+print("::::: Your Score :::::")
+print("Foundation English :",fe)
+print("General Business :",gb)
+print("Introduction to Computer Systems :",ics)
+print("Computer Programming :",cp)
