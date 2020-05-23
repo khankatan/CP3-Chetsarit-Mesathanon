3fd6e3050f386328fb5d738e26f9e5ff5cf2cb96
khankatan <khan71037@gmail.com>
2020-05-23 22:52:09 +0700

    test

---
+++ assignments/test login.py
@@ -0,0 +1,9 @@
+print("===== L O N G I N =====")
+user1 = input("Username : ")
+pass1 = input("Password : ")
+print("----- ----------- -----")
+if user1 == "chetsarit" and pass1 == "71037":
+    print("Welcome to My world")
+else:
+    print("Error!")
+print("===== =========== ======")
---
+++ assignments/testGPA.py
@@ -0,0 +1,19 @@
+score = int(input("YOUR SCORE : "))
+if score > 100:
+    print("ERROR!!!")
+elif score >= 80:
+    print("GRAD A")
+elif score >= 75:
+    print("GRAD B+")
+elif score >= 70:
+    print("GRAD B")
+elif score >= 65:
+    print("GRAD C+")
+elif score >= 60:
+    print("GRAD C")
+elif score >= 55:
+    print("GRAD D+")
+elif score >= 50:
+    print("GRAD D")
+elif score < 50:
+    print("GRAD F")
